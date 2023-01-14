c06ba61a846e352a079be127b4f8fbb55ea45a5b
jgrygoryeva@gmail.com <jgrygoryeva@gmail.com>
2023-01-13 18:33:28 -0800

    Add empty project

---
+++ CatWidget/CatWidget/CatWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatWidgetApp.swift
+//  CatWidget
+//
+//  Created by Yuliya  on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct CatWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
