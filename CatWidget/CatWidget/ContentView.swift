d7416b0abc0a62a73493339f307ed3c473d2858d
jgrygoryeva@gmail.com <jgrygoryeva@gmail.com>
2023-01-13 19:46:02 -0800

    Add widget target

--- CatWidget/CatWidget/ContentView.swift
+++ CatWidget/CatWidget/ContentView.swift
@@ -10,11 +10,23 @@ import SwiftUI
 let cats = [cat1, cat2, cat3]
 
 struct ContentView: View {
+    
+    @AppStorage("cat", store: UserDefaults(suiteName: "group.Yuliya-Grygoryeva.CatWidget"))
+    var catData: Data = Data()
     var body: some View {
         VStack {
             ForEach(cats) { cat in
                 CatView(cat: cat)
+                    .onTapGesture {
+                        saveToDefaults(cat: cat)
+                    }
             }
+        }
+    }
+    
+    func saveToDefaults(cat: Cat){
+        if let catData = try? JSONEncoder().encode(cat) {
+            self.catData = catData
         }
     }
 }
---
+++ CatWidget/WidgetCat/WidgetCatBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetCatBundle.swift
+//  WidgetCat
+//
+//  Created by Yuliya  on 1/13/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetCatBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetCat()
+        WidgetCatLiveActivity()
+    }
+}
